3d7ef7d17913c9cda00e2d86eecc68c34f06a116
jusin-PC\Just <justincheung6@gmail.com>
2019-06-14 17:48:53 -0700

    Created the Damage Giver script

--- BadGuySmackers/Assets/Scripts/Health/DamageGiver.cs
+++ BadGuySmackers/Assets/Scripts/Health/DamageGiver.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class DamageGiver : MonoBehaviour
 {
+    [SerializeField] protected float damageValue = 25f;
 
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        //Check if collided object can take damage
+        DamageTaker_Abstract taker = col.gameObject.GetComponent<DamageTaker_Abstract>();
+        if(taker != null)
+        {
+            //Apply damage to the character
+            taker.TakeDamage(this, damageValue);
+        }
+    }
 }
